78425859a433a6598a58f45754dfe0ba8b1f270f
Jacob Birkeland <birkeland02@gmail.com>
2021-10-22 15:59:29 -0500

    added GalleryList and GalleryItem css files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 function App() {
+
+  useEffect(() => {
+    fetchList()
+  }, []) //end useEffect
+
+  let [galleryList, setGalleryList] = useState([]);
+
+  const fetchList = () => {
+    axios({
+      method: 'GET',
+      url: '/list'
+    }).then((response) => {
+      console.log('entire response', response);
+      setGalleryList(response.data)
+    }).catch(function (error) {
+      console.log('Error on GET', error);
+    })//end fetchList/GET
+  }
+
+  const addItem = (newItem) => {
+    console.log(newItem);
+    axios({
+      method: 'POST',
+      url: '/list',
+      data: newItem
+    }).then(response => {
+      console.log('response is', response);
+      fetchList();
+    }).catch(error => {
+      console.log('Error in POST', error);
+    });
+  }; //end addItem/POST  
+
+  
+
     return (
       <div className="App">
         <header className="App-header">
